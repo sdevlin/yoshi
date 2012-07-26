9f3a63c57e8d0542126196db3a7950f73ca4a9e3
Sean Devlin <devlin@alumni.nd.edu>
2012-07-25 21:10:40 -0400

    sigh, starting over

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static void eval(void *exp, void *env);
+
 int main(int argc, char **argv) {
+  eval(NULL, NULL);
   return 0;
+}
+
+static void eval(void *exp, void *env) {
+
 }
