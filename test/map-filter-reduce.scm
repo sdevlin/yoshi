1a4e9a22abf859394a74a1b1f89c3916c0cb779b
Sean Devlin <devlin@alumni.nd.edu>
2012-08-12 18:45:15 -0400

    changed map/filter/reduce test to more idiomatic scheme

--- test/map-filter-reduce.scm
+++ test/map-filter-reduce.scm
@@ -1,28 +1,26 @@
 (define map
   (lambda (f l)
-    (if (null? l)
-        ()
-        (cons (f (car l))
-              (map f (cdr l))))))
+    (cond
+     ((null? l) ())
+     (else (cons (f (car l))
+                 (map f (cdr l)))))))
 
-(map (lambda (x) (* x x x))
-     (list 1 2 3 4 5))
+(map (lambda (x) (* x x x)) '(1 2 3 4 5))
 
 (define filter
   (lambda (f l)
-    (if (null? l)
-        ()
-        (if (f (car l))
-            (cons (car l) (filter f (cdr l)))
-            (filter f (cdr l))))))
+    (cond
+     ((null? l) '())
+     ((f (car l))
+      (cons (car l) (filter f (cdr l))))
+     (else (filter f (cdr l))))))
 
-(filter (lambda (x) (= (mod x 2) 0))
-        (list 1 2 3 4 5 6 7))
+(filter (lambda (x) (= (mod x 2) 0)) '(1 2 3 4 5 6 7))
 
 (define reduce
   (lambda (f l acc)
-    (if (null? l)
-        acc
-        (reduce f (cdr l) (f (car l) acc)))))
+    (cond
+     ((null? l) acc)
+     (else (reduce f (cdr l) (f (car l) acc))))))
 
-(reduce + (list 1 2 3 4 5) 0)
+(reduce + '(1 2 3 4 5) 0)
